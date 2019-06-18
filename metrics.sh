ae6b7ed5dc95c58f6a99648cca3f55359d50ca20
DrAntoine <antoine.druart@me.com>
2019-06-18 13:58:48 +0200

    adapt metrics.sh for server usage

--- metrics.sh
+++ metrics.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/env bash
 
+#$ -N binning_metrics
+#$ -M antoine.druart@me.com
+#$ -m seab
+#$ -cwd   #Use the directory you're running from
+#$ -l h_rt=0:15:0,h_vmem=2G   #Setting running time in hours:min:sec and the memory required for the job
+#$ -j y   #Joining the output from standard out and standard error to one file
+#$ -pe smp 6-24   #Setting the number of threads for the job to best fit for the system between 1 and N.
+#$ -e binning-metrics.log
+#$ -o binning-metrics.log
+#$ -V
+
 module load biopython
 module load python/3.7.3
 module load andi/0.12
